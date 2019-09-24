e49146b2bbfd477d19484e289ebea03f4e42e349
uhyo <uhyo@uhy.ooo>
2019-09-25 00:06:05 +0900

    add next program to test

--- packages/jelly-parser/src/__tests__/parseProgram.ts
+++ packages/jelly-parser/src/__tests__/parseProgram.ts
@@ -4,4 +4,7 @@ describe("parseProgram", () => {
   it("ɠḲV×/ị“Odd“Even”", () => {
     expect(parseProgram("ɠḲV×/ị“Odd“Even”")).toMatchSnapshot();
   });
+  it("ɠċ”1", () => {
+    expect(parseProgram("ɠċ”1")).toMatchSnapshot();
+  });
 });
